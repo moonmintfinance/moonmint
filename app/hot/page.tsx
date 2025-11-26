c3c864d3261da527f4f45066cb3c31ad93039d07
chadmintteam <contact@moonmint.finance>
2025-11-26 05:00:26 -0800

    2.2.120

--- app/hot/page.tsx
+++ app/hot/page.tsx
@@ -2,14 +2,14 @@
 
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
-import { Hot25Tokens } from '@/components/Hot25Tokens';
+import { HotTokens } from '@/components/Hot25Tokens';
 
 export default function Hot25Page() {
   return (
     <main className="min-h-screen flex flex-col bg-gradient-dark">
       <Header />
       <div className="flex-grow">
-        <Hot25Tokens />
+        <HotTokens />
       </div>
       <Footer />
     </main>
